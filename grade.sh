39b4f327c2b8c2d47045b8e62233fadbe361130f
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2023-02-15 16:38:06 -0800

    change bash code

--- grade.sh
+++ grade.sh
@@ -1,5 +1,31 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission
 echo 'Finished cloning'
+
+if [[ -f ListExamples.java ]]
+then
+    echo "ListExamples found"
+else
+    echo "need file ListExamples.java"
+    exit 1
+fi
+
+cp ../TestListExamples.java .
+cp -r ../lib .
+javac -cp $CPATH *.java
+
+echo "Finished compiling"
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
+
+if [[ `grep -h "OK" output.txt` != "" ]] 
+then
+    echo "All tests passed"
+    echo `grep -h "test" output.txt`
+else
+    echo `grep -h "Tests run" output.txt`
+    echo "testMergeRightEnd failed"
+fi
